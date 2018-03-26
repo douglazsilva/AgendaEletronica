a21b40ae3ebad5b5d9b178b8e4d87f8f2ceb9161
douglazsilva <fdcsone@hotmail.com>
2018-03-26 15:21:37 -0300

    Adicionando formulário

--- src/main/java/tk/douglazsilva/agendaeletronica/Criar.java
+++ src/main/java/tk/douglazsilva/agendaeletronica/Criar.java
@@ -1,6 +1,11 @@
 package tk.douglazsilva.agendaeletronica;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.CompoundPropertyModel;
+
+import tk.douglazsilva.agendaeletronica.contato.Contato;
 
 public class Criar extends BasePage{	
 	
@@ -8,6 +13,26 @@ public class Criar extends BasePage{
 
 	public Criar() {
 		add(new Label("titulo", "Criação de Contato"));
+		
+		Contato contato = new Contato();
+		CompoundPropertyModel<Contato> compoundPropertyModelContato = new CompoundPropertyModel<Contato>(contato);
+		Form<Contato> form = new Form<Contato>("formularioContato",compoundPropertyModelContato) {			
+			private static final long serialVersionUID = -5576287141403246286L;
+			
+			@Override
+			protected void onSubmit() {
+				Contato contatoSubmetido = getModelObject();
+				System.out.println("Contato a inserir: " + contatoSubmetido);
+				setResponsePage(Inicio.class);
+			}
+			
+		};
+		add(form);
+		
+		TextField<String> inputNome = new TextField<String>("nome");
+		TextField<String> inputEmail = new TextField<String>("email");
+		TextField<String> inputTelefone = new TextField<String>("telefone");
+		form.add(inputNome,inputEmail, inputTelefone);
 	}
 
 }
